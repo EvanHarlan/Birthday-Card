cc8dd6952ec31c26395a4a6213215466c14c12a0
Evan Harlan <evanharlan10@gmail.com>
2024-07-01 11:12:11 -0500

    Added the index and main styles as well as dark/light mode toggle

---
+++ toggleTheme.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleSwitch = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    toggleSwitch.addEventListener('change', (e) => {
+        if (e.target.checked) {
+            document.body.classList.add('dark-mode');
+            document.body.classList.remove('light-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.add('light-mode');
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
